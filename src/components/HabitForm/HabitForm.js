1a827251963604673e2d7363a98bade41478f2f4
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-12-06 11:26:52 -0700

    Update styling, delete commented out code, & update how HabitCard render;

--- src/components/HabitForm/HabitForm.js
+++ src/components/HabitForm/HabitForm.js
@@ -1,58 +1,44 @@
 import "./HabitForm.css"
 import { AppContext } from "../../utils/context.js"
 import { QUERY_HABITS } from "../../utils/graph_queries"
-import { useQuery } from "@apollo/client"
+import { SUBMIT_HABIT } from "../../utils/graph_mutations"
+import { useQuery, useMutation } from "@apollo/client"
 import { useEffect, useContext } from "react"
 import HabitCard from "../HabitCard/HabitCard"
-import { useMutation } from "@apollo/client"
-import { SUBMIT_HABIT } from "../../utils/graph_mutations"
 import "../../assets/icons/habit7-uncheck.png"
 
 const HabitForm = () => {
   const { loading, error, data } = useQuery(QUERY_HABITS)
-  const { userHabits, setUserHabits, checkedHabitId } = useContext(AppContext)
-  const [createHabitEntry] = useMutation(SUBMIT_HABIT)
-
-  // const [checkedCards, setCheckedCards] = useState([])
+  const { userHabits, setUserHabits, checkedHabitIds } = useContext(AppContext)
+  const [ createHabitEntry ] = useMutation(SUBMIT_HABIT)
 
   useEffect(() => {
     if (!loading && data) {
-      setUserHabits(data.fetchHabit)
+      setUserHabits(data.fetchHabits)
     }
-  }, [data])
-
-  // useEffect(() => {
-  //   const checkedHabits = checkedHabitId.map(ele => {
-  //     return userHabits.find(habit => habit.id === ele)
-  //   })
-  //   const checkedHabitCards = checkedHabits.map(habit => <HabitCard name={habit.name} id={habit.id} key={habit.id} />)
-  //   setCheckedCards(checkedHabitCards)
-  // }, [checkedHabitId])
-
-  const habitCards = userHabits.map(habit => <HabitCard name={habit.name} id={habit.id} key={habit.id} />)
+  }, [ data, loading ])
 
   const createHabitEntries = e => {
     e.preventDefault()
-    const entryParams = checkedHabitId.map(ele => ({ id: ele }))
+    const entryParams = checkedHabitIds.map(ele => ({ id: ele }))
     if (!entryParams.length) {
       alert("You have not selected any habit")
     } else {
       createHabitEntry({ variables: { idArr: entryParams } })
       alert("Great job")
-      // setHabitSubmitted(true)
     }
   }
 
-  return habitCards ? (
-    <div className="habit-form-container">
-      <h2>What have you accomplished?</h2>
-
-      <form className="habit-form"> {habitCards}</form>
-
+  return userHabits ? (
+    <section className="habit-form-container">
+      <h2 className="habit-form-question">What have you accomplished?</h2>
+      <form className="habit-form">
+        { userHabits.map(habit => <HabitCard name={habit.name} id={habit.id} key={habit.id} />) }
+      </form>
       <button className="habit-submit-button" onClick={createHabitEntries}>
         Submit
       </button>
-    </div>
+    </section>
   ) : (
     <h2>{error}</h2>
   )
