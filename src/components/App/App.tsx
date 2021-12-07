da74def9376c97b43e900cac7ff48c9ff87946c6
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-12-07 18:47:49 -0500

    Change homepage to dashboard. Display mood and habit tracker on one page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import "./App.css"
 import MoodForm from "../MoodForm/MoodForm"
 import HabitForm from "../HabitForm/HabitForm"
 import Dashboard from "../Dashboard/Dashboard"
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom"
 
 function App() {
   return (
@@ -10,29 +10,26 @@ function App() {
       <header className="header">
         <h1>GlowUp</h1>
         <nav className="nav">
-          <NavLink to="glow-up-fe/">
-            Mood Tracker
-          </NavLink>
-          <NavLink to="glow-up-fe/habit-tracker">
-            Habit Tracker
-          </NavLink>
-          <NavLink to="glow-up-fe/journal">
-            Journal
-          </NavLink>
-          <NavLink to="glow-up-fe/dashboard">
-            Dashboard
-          </NavLink>
-
+          <NavLink to="glow-up-fe/dashboard">Dashboard</NavLink>
+          <NavLink to="glow-up-fe/track">Track</NavLink>
+          <NavLink to="glow-up-fe/journal">Journal</NavLink>
         </nav>
       </header>
       <Routes>
         <Route path="glow-up-fe">
-          <Route index element={<MoodForm />}/>
-          <Route path="habit-tracker" element={<HabitForm />}/>
-          <Route path="dashboard" element={<Dashboard />}/>
+          <Route index element={<Dashboard />} />
+          <Route
+            path="track"
+            element={
+              <section className="trackers-container">
+                <MoodForm />
+                <HabitForm />
+              </section>
+            }
+          />
+          <Route path="dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
-
     </div>
   )
 }
