7aa2f01b165fb819566ecbba862918dd1364a02a
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-12-06 20:32:59 -0500

    Add context moodRecorded and habitRecored. Conditional rendering on dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,27 @@
-import './Dashboard.css'
+import "./Dashboard.css"
+import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { AppContext } from "../../utils/context"
+// import { QUERY_HABITS } from "../../utils/graph_queries"
 
 const Dashboard = () => {
+  const { moodRecorded, habitRecorded } = useContext(AppContext)
 
-    return (
-      <section className="dashboard-container">
-        <h2>My Dashboard</h2>
-
-      </section>
-    )
+  return (
+    <section className="dashboard-container">
+      <h2>My Dashboard</h2>
+      {moodRecorded ? (
+        <div className="daily-mood">🥳 I feel super today</div>
+      ) : (
+        <Link to="glow-up-fe/">Enter your mood today!</Link>
+      )}
+      {habitRecorded ? (
+        <div className="daily-habit">Today I've done blah blah</div>
+      ) : (
+        <Link to="glow-up-fe/habit-tracker">Enter your habits today!</Link>
+      )}
+    </section>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
--- src/utils/context.tsx
+++ src/utils/context.tsx
@@ -3,24 +3,46 @@ import { Habit } from "./Models"
 
 interface ContextState {
   userHabits: Habit[]
-  checkedHabitIds: number[]
   setUserHabits: (habits: Habit[]) => void
+  checkedHabitIds: number[]
   setCheckedHabitIds: (habitIds: number[]) => void
+  moodRecorded: boolean
+  setMoodRecorded: (moodRecorded: boolean) => void
+  habitRecorded: boolean
+  setHabitRecorded: (habitRecorded: boolean) => void
 }
 
 const AppContext = createContext<ContextState>({
   userHabits: [],
-  checkedHabitIds: [],
   setUserHabits: () => {},
+  checkedHabitIds: [],
   setCheckedHabitIds: () => {},
+  moodRecorded: false,
+  setMoodRecorded: () => {},
+  habitRecorded: false,
+  setHabitRecorded: () => {},
 })
 
 const ContextProvider = ({ children }: React.PropsWithChildren<{}>) => {
   const [userHabits, setUserHabits] = useState<Habit[]>([])
   const [checkedHabitIds, setCheckedHabitIds] = useState<number[]>([])
 
+  const [moodRecorded, setMoodRecorded] = useState<boolean>(false)
+  const [habitRecorded, setHabitRecorded] = useState<boolean>(false)
+
   return (
-    <AppContext.Provider value={{ userHabits, setUserHabits, checkedHabitIds, setCheckedHabitIds }}>
+    <AppContext.Provider
+      value={{
+        userHabits,
+        setUserHabits,
+        checkedHabitIds,
+        setCheckedHabitIds,
+        moodRecorded,
+        setMoodRecorded,
+        habitRecorded,
+        setHabitRecorded,
+      }}
+    >
       {children}
     </AppContext.Provider>
   )
