db4a442bb7f2f5fe9ec8663989c7184ebafe19cf
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-12-10 14:07:58 -0500

    Add mock data for test. Add test for today's mood if recorded

--- cypress/integration/Track.spec.js
+++ cypress/integration/Track.spec.js
@@ -9,4 +9,9 @@ describe("Track page", () => {
       }
     })
   })
+
+  it("If user has filled in today's mood, user should see the mood in track page", () => {
+    cy.get(".today-mood-container > h3").contains("Today")
+    cy.get(".today-mood-container").children().eq(2).contains("😁")
+  })
 })
