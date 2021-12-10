f48584883ab57b037ae3ff585a6920e7627f7a49
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-12-10 10:26:36 -0500

    Add some styles to weekly cards

--- src/components/MoodForm/MoodForm.tsx
+++ src/components/MoodForm/MoodForm.tsx
@@ -30,7 +30,7 @@ const MoodForm = () => {
 
   return (
     <section className="mood-form-container">
-      {todaysMood || isSubmitted ? (
+      {isSubmitted || todaysMood ? (
         <>
           <MoodToday />
           {todaysMood!.mood <= 2 ? <Quote /> : null}
