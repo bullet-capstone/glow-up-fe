5b3af202017903dad5bb2b9a3c5aba3e7b62b15c
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-12-10 22:01:01 -0700

    Update graphql utils & write tests for Journal view

--- cypress/utils/graphql-test-utils.js
+++ cypress/utils/graphql-test-utils.js
@@ -1,8 +1,7 @@
-import mockUserData from '../fixtures/mockUserData.json'
-
 // Utility to match GraphQL mutation based on the operation name
 export const hasOperationName = (req, operationName) => {
   const { body } = req
+  console.log("body", body)
   return (
     body.hasOwnProperty('operationName') && body.operationName === operationName
   )
@@ -10,19 +9,9 @@ export const hasOperationName = (req, operationName) => {
 
 // Alias query if operationName matches
 export const aliasQuery = (req, operationName) => {
-  if (hasOperationName(req, operationName) && operationName === 'fetchUser') {
+  if (hasOperationName(req, operationName)) {
     req.alias = `gql${operationName}Query`
 
-    req.reply(res => {
-     res.body = mockUserData;
-    });
-  }
-
-  if (hasOperationName(req, operationName) && operationName === 'fetchHabits') {
-    req.alias = `gql${operationName}Query`
-
-    req.reply(res => {
-     res.body = mockUserData;
-    });
+    req.reply({ fixture: 'mockUserData.json'});
   }
 }
