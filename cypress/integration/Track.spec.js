42c44a05380cede367259e1100d5f5af0db64454
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-12-12 16:14:14 -0500

    Adopt test util fxn. Add test for today's date

--- cypress/integration/Track.spec.js
+++ cypress/integration/Track.spec.js
@@ -1,16 +1,27 @@
-describe("Track page", () => {
+import { aliasQuery } from "../utils/graphql-test-utils"
+
+describe("Mood form happy path", () => {
   beforeEach(() => {
-    cy.intercept("POST", "/graphql", req => {
-      if (req.body.operationName === "FetchDailyEntries") {
-        req.alias = "gql-daily-entries"
-        req.reply({ fixture: "mockDailyEntries.json" })
-      }
+    cy.intercept("POST", "http://localhost:3001/graphql", req => aliasQuery(req, "FetchDailyEntries"))
+
+    cy.visit("/track")
+
+    cy.wait("@gqlFetchDailyEntriesQuery").then(interception => {
+      expect(interception).to.be.an("object")
     })
+
+    cy.intercept("GET", "https://api.quotable.io/random", { fixture: "mockQuote.json" }).as("getMockQuote")
   })
 
-  it("If user has filled in today's mood, user should see the mood in track page", () => {
-    cy.visit("http://localhost:3000/glow-up-fe/track")
-    cy.get(".today-mood-container > h3").contains("Today")
-    cy.get(".today-mood-container > :nth-child(2)").contains("😁")
+  it("If user has filled in today's mood, user should see today's date and recorded mood", () => {
+    let day = new Date()
+    var dd = String(day.getDate()).padStart(2, "0")
+    var mm = String(day.getMonth() + 1).padStart(2, "0")
+    var yyyy = day.getFullYear()
+
+    cy.get(".today-mood-container > h3").contains(`${yyyy}-${mm}-${dd}`)
+    cy.get(".today-mood-container > :nth-child(2)").contains("😐")
   })
+
+  it("If user's mood is below or equal 2, user should see a quote to cheer user up", () => {})
 })
--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -5,7 +5,7 @@ export default function Quote() {
   const [author, setAuthor] = useState("")
 
   useEffect(() => {
-    fetch("https://api.quotable.io/random?")
+    fetch("https://api.quotable.io/random")
       .then(response => response.json())
       .then(data => {
         setQuote(data.content)
