f68a5c7dcab2cc4a5c07a28da53f7314ffe18e67
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-12-12 20:00:26 -0500

    Fix small bug regarding mood description

--- src/components/MoodToday/MoodToday.tsx
+++ src/components/MoodToday/MoodToday.tsx
@@ -2,11 +2,6 @@ import { useContext } from "react"
 import { AppContext } from "../../utils/context"
 import "./MoodToday.css"
 
-// interface MoodTodayProps {
-//   mood: number
-//   description: string | undefined
-// }
-
 export default function MoodToday() {
   const { todaysMood, displayMood, getDayString } = useContext(AppContext)
   const date = getDayString(0)
@@ -14,7 +9,7 @@ export default function MoodToday() {
     <div className="today-mood-container">
       <h3>Today: {date}</h3>
       <p>I am feeling: {displayMood(todaysMood!.mood)}</p>
-      <p>{todaysMood!.description}</p>
+      {todaysMood!.description && <p>{todaysMood!.description}</p>}
       {/* <button>Second Thought?</button>  to add when changing mood fnc is available*/}
     </div>
   )
