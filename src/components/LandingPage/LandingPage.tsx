6250c82d5bdb175ff97b28a7e8668bfb1ea3c3ec
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-22 22:28:59 -0500

    Add button styles

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,31 +1,30 @@
 import "./LandingPage.css"
 import logo from '../../assets/logo-transparent.png'
 import { Link } from "react-router-dom"
+import { InputAdornment } from '@mui/material';
 
 export default function LandingPage() {
   return (
     <div className="landing-view">
-      <div className="landing-header">
+      <section className="landing-header">
         <img id="logo-img" src={logo} alt="logo" />
-        <h4>Glow Up</h4>
-        <ul className="landing-ul">
-          <li>Home</li>
-          <li>Product</li>
-          <li>Team</li>
-        </ul>
-        
-        
-      </div>
-      <div className="landing-main">
+        {/* <h4>Glow Up</h4> */}
+      </section>
+      <section className="landing-main">
+        <div className="landing-intro">
+          <h1>Landing template for starups</h1>
+          <h3>Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever. </h3>
+        </div>
+     
         <div className="landing-btn-container">
           <Link to="/glow-up-fe/signup">
-            <button className="signup-button">Sign Up</button>
+            <button className="signup-button">SIGN UP</button>
           </Link>
           <Link to="/glow-up-fe/login">
-        <button className="login-button">Login</button>
+        <button className="login-button">LOGIN</button>
         </Link>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
