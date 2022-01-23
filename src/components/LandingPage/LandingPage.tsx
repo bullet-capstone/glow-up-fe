f8fe29f0c43eb819833409c0a24c72bcf4b31485
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-23 13:20:07 -0500

    Add some styles to landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -10,10 +10,10 @@ export default function LandingPage() {
         {/* <h4>Glow Up</h4> */}
       </section>
       <section className="landing-main">
-        <div className="landing-intro">
-          <h1>Landing template for starups</h1>
-          <h3>Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever. </h3>
-        </div>
+        {/* <div className="landing-intro"> */}
+          <h1 className="landing-title">Landing template for starups</h1>
+          <h3 className="landing-intro">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever. </h3>
+        {/* </div> */}
      
         <div className="landing-btn-container">
           <Link to="/glow-up-fe/login">
