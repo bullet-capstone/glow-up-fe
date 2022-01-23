1f846669605344dd3c912ec68ff33edce98301fc
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-22 22:06:48 -0500

    Add logo image to landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,28 +1,30 @@
 import "./LandingPage.css"
+import logo from '../../assets/logo-transparent.png'
 import { Link } from "react-router-dom"
 
 export default function LandingPage() {
   return (
-    <div className="landing-container">
-      <div className="landing-nav">
+    <div className="landing-view">
+      <div className="landing-header">
+        <img id="logo-img" src={logo} alt="logo" />
         <h4>Glow Up</h4>
         <ul className="landing-ul">
           <li>Home</li>
           <li>Product</li>
           <li>Team</li>
         </ul>
-        <Link to="/glow-up-fe/login">
-        <button className="login-button">Login</button>
-        </Link>
+        
         
       </div>
       <div className="landing-main">
-        <div className="main-left">
+        <div className="landing-btn-container">
           <Link to="/glow-up-fe/signup">
             <button className="signup-button">Sign Up</button>
           </Link>
+          <Link to="/glow-up-fe/login">
+        <button className="login-button">Login</button>
+        </Link>
         </div>
-        <div className="main-right"></div>
       </div>
     </div>
   )
