0bbac4043814577d2d270ba1be66dd928456129a
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-28 11:02:03 -0500

    Add function to logout button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,8 @@ import MenuItem from '@mui/material/MenuItem';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { NavLink } from 'react-router-dom';
+import { NavLink,useNavigate } from 'react-router-dom';
+import { useCookies } from "react-cookie";
 import './Header.css'
 
 const theme = createTheme({
@@ -30,11 +31,12 @@ const theme = createTheme({
 });
 
 const pages = ['dashboard', 'track', 'journal'];
-const settings = ['Logout'];
 
 const Header = () => {
+  const navigate = useNavigate()
+  const [cookie,]= useCookies(['userToken'])
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+ 
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -43,11 +45,12 @@ const Header = () => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElUser(event.currentTarget);
-  // };
+ 
   const handleLogout = () => {
     console.log('logout fires');
+    // call logout mutation, remove cookie
+    // redirect to login page
+    navigate('/glow-up-fe/')
     
   };
 
@@ -134,28 +137,6 @@ const Header = () => {
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
               </Tooltip>
-            {/* <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
           </Box>
         </Toolbar>
       </Container>
