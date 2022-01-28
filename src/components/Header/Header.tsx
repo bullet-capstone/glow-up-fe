af0453863011be833b7a9929952529e2eced8093
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-28 10:56:50 -0500

    Add logout button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,8 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { NavLink } from 'react-router-dom';
 import './Header.css'
@@ -28,9 +30,11 @@ const theme = createTheme({
 });
 
 const pages = ['dashboard', 'track', 'journal'];
+const settings = ['Logout'];
 
 const Header = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -38,6 +42,13 @@ const Header = () => {
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
+  };
+  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
+  const handleLogout = () => {
+    console.log('logout fires');
+    
   };
 
   return (
@@ -105,7 +116,7 @@ const Header = () => {
             GlowUp
           </Typography>
           <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-            {/* {userToken ? */}
+            
             {pages.map((page) => (
               <NavLink to={`/glow-up-fe/${page}`} key={page}>
                 <Button
@@ -114,12 +125,37 @@ const Header = () => {
                 >
                   {page}
                 </Button>
-                {/* 'Logout' 
-                :
-              ['login', 'signup']
-            } */}
               </NavLink>
             ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Log out">
+              <IconButton onClick={handleLogout} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+              </Tooltip>
+            {/* <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu> */}
           </Box>
         </Toolbar>
       </Container>
