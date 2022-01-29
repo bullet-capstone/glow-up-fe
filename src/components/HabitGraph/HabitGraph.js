67aea1f644d2f68d99a35c880b9784d224c1bdd7
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-29 13:02:24 -0500

    Change habit graph axis

--- src/components/HabitGraph/HabitGraph.js
+++ src/components/HabitGraph/HabitGraph.js
@@ -8,7 +8,8 @@ import {
 import { ScatterChart,} from "echarts/charts";
 import { UniversalTransition } from "echarts/features";
 import { CanvasRenderer } from "echarts/renderers";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useContext } from "react";
+import { AppContext } from "../../utils/context"
 
 echarts.use([
   TitleComponent,
@@ -33,41 +34,41 @@ const Graph = () => {
   let myChart = null;
 
   const hours = [
-    "12a",
-    "1a",
-    "2a",
-    "3a",
-    "4a",
-    "5a",
-    "6a",
-    "7a",
-    "8a",
-    "9a",
-    "10a",
-    "11a",
-    "12p",
-    "1p",
-    "2p",
-    "3p",
-    "4p",
-    "5p",
-    "6p",
-    "7p",
-    "8p",
-    "9p",
-    "10p",
-    "11p"
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "16",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31"
   ];
+  const {habitList} = useContext(AppContext)
+  const days = habitList.map(habit => habit.name)
 
-  const days = [
-    "Saturday",
-    "Friday",
-    "Thursday",
-    "Wednesday",
-    "Tuesday",
-    "Monday",
-    "Sunday"
-  ];
 
   const data = [
     [0, 0, 5],
